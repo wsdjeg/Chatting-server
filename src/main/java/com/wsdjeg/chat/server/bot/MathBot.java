4d705d119b37437e22520857c565f5d42853de5b
wsdjeg <wsdjeg@outlook.com>
2016-12-09 21:43:10 +0800

    Add:get_list_by_sum

--- src/main/java/com/wsdjeg/chat/server/bot/MathBot.java
+++ src/main/java/com/wsdjeg/chat/server/bot/MathBot.java
@@ -1,7 +1,40 @@
 package com.wsdjeg.chat.server.bot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wsdjeg.chat.server.Command;
+import com.wsdjeg.chat.server.util.MathUtils;
+
 public class MathBot implements Bot{
+    private String GET_LIST_BY_SUM = "/get_list_by_sum";
+    private List<Integer> integerList = new ArrayList<>();
+    private String type;
+
     public String reply(String msg){
-        return "hi, I am MathBot!";
+        if (msg.equals(GET_LIST_BY_SUM)) {
+            type = GET_LIST_BY_SUM;
+            return "MathBot type is " + type;
+        }
+        if (type != null) {
+            return reply(type, msg);
+        }else{
+            return "you need select a type.";
+        }
+    }
+    private String reply(String type, String msg){
+        if (type.equals(GET_LIST_BY_SUM)
+                && isInteger(msg)) {
+            integerList.add(Integer.valueOf(msg));
+            return msg + " is added into list!";
+        }else if (type.equals(GET_LIST_BY_SUM)
+                && msg.matches("^/sum [1-9][0-9]*$")) {
+            return "sum list is : " + MathUtils.getResult(integerList, Integer.valueOf(msg.split(Command.SPLIT)[1]));
+        }
+        return null;
+    }
+
+    private boolean isInteger(String i){
+        return i.matches("^[1-9][0-9]*$");
     }
 }
--- src/main/java/com/wsdjeg/chat/server/util/MathUtils.java
+++ src/main/java/com/wsdjeg/chat/server/util/MathUtils.java
@@ -5,10 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MathUtils {
-    public static List<List<Integer>> get_list_by_sum(List<Integer> list, int sum){
-        return null;
-    }
-    public String getResult(List<Integer> list,int sum){
+    public static String getResult(List<Integer> list,int sum){
         int min = 1;
         int max = list.size();
         int sumtemp = 0;
@@ -37,20 +34,15 @@ public class MathUtils {
         }
         return result;
     }
-    /**
-     * 从一个List中选取n各元素 所有可能的组合排列
-     */
-    public String getSubListWithnum(List<Integer> list,int n,int sumTemp,int sum,String result,String results,int index){
+    private static String getSubListWithnum(List<Integer> list,int n,int sumTemp,int sum,String result,String results,int index){
         results = "";
         if (n > 1 && n <= list.size()) {
             for (int i = index ; i < list.size() - (n-1); i++) {
                 int minSumTemp = 0;
                 int maxSumTemp = 0;
-                //从list中取n各数,目前已经取一个,还需取n-1个(从listTemp中取),从0 取到n-1-1 即为 < n -1
                 for (int i1 = index + 1  ; i1 < index + n - 1; i1++) {
                     minSumTemp += list.get(i1);
                 }
-                //从 倒数第n-1个取到 最后一个 listTemp.size() -i = n-1
                 for (int i1 = list.size() - (n-1) ; i1 < list.size() ; i1++){
                     maxSumTemp += list.get(i1);
                 }
@@ -63,9 +55,7 @@ public class MathUtils {
         }else if(n == 1){
             for (int k = index ; k < list.size(); k++) {
                 if (sumTemp + list.get(k) ==sum) {
-                    //System.out.println(sumTemp+ ","+ list.get(k) +"," +sum + "," +result +"," + results);
-                    results += "[" + result  + list.get(k) + "]<br>";
-                    //System.out.println(sumTemp+ ","+ list.get(k) +"," +sum + "," +result +"," + results);
+                    results += "[" + result  + list.get(k) + "];";
                 }
             }
             return results;
