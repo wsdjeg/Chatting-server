5bb1fcdd0730addca486d52aa2cff383fc4d0373
wsdjeg <wsdjeg@outlook.com>
2016-12-11 14:28:48 +0800

    Fix args parser

--- src/main/java/com/wsdjeg/chat/Server.java
+++ src/main/java/com/wsdjeg/chat/Server.java
@@ -11,17 +11,36 @@ import com.wsdjeg.chat.server.Account;
 import com.wsdjeg.chat.server.ServerThread;
 
 public class Server extends ServerSocket {
-    private static final int SERVER_PORT = 2013;
-    public static final String version = "0.1.0";
+    private static boolean debugMode = false;
+    private static int SERVER_PORT = 2013;
+    private static String databaseFileName = "";
+    public static final String VERSION = "0.1.0";
 
     public static void main (String[] args) throws IOException{
-        int port = 0;
-        if (port > 1023 && args.length == 1) {
-            port = Integer.valueOf(args[0]);
-            new Server(port);
-        }else{
-            new Server();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            switch (arg) {
+                case "-h":
+                    usage();
+                    return;
+                case "-v":
+                    version();
+                    return;
+                case "--database":
+                    databaseFileName = args[++i];
+                    break;
+                case "-d":
+                    debugMode = true;
+                    break;
+                case "-D":
+                    SERVER_PORT = Integer.valueOf(args[++i]);
+                    break;
+                case "-port":
+                    SERVER_PORT = Integer.valueOf(args[++i]);
+                    break;
+            }
         }
+        new Server();
     }
 
     public Server() throws IOException {
@@ -47,6 +66,16 @@ public class Server extends ServerSocket {
         }
     }
 
-
-
+    public static void usage(){
+        version();
+        System.out.println("  java [-classpath] com.wsdjeg.chat.Server [-h] [-v] [--database databaseFileName] [-d] [-D port]");
+        System.out.println("Options:");
+        System.out.println("  -h	        help");
+        System.out.println("  -v	        version");
+        System.out.println("  -d            enable debug mode");
+        System.out.println("  -D port       start daemon on specified port");
+    }
+    public static void version(){
+        System.out.println("Chatting server V" + VERSION);
+    }
 }
---
+++ src/main/java/com/wsdjeg/chat/server/message/MessageBuilder.java
@@ -0,0 +1,9 @@
+package com.wsdjeg.chat.server.message;
+
+import java.util.Map;
+
+public class MessageBuilder {
+    public static Map<String,String> build(){
+        return null;
+    }
+}
